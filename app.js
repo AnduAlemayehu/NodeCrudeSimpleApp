5e366958a88a571fb6ab0b895eab5180da1df5d8
Andu <andualemalemayehuwku@gmail.com>
2022-12-30 18:59:28 +0300

    data inserting from html form in to database code finished

--- app.js
+++ app.js
@@ -4,24 +4,45 @@ const serverApp = express();
 require("dotenv").config();
 const PORT = process.env.PORT;
 
+// Middle ware to extract info from the html
+serverApp.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+//serverApp.use(express.json());
+
+//create db connection params
 
 const con = mysql.createConnection({
   host: "localhost",
   user: process.env.USER,
   password: process.env.PASSWORD,
-  database: process.env.DATABASE
+  database: process.env.DATABASE,
 });
+
+// db connecting
 
 con.connect(function (err) {
   if (err) throw err;
   console.log("Connected!");
 });
+
+// Route: / => Homepage route
+
+serverApp.get("/", (req, res) => res.send("application running"));
+
+//port listening
+
 serverApp.listen(PORT, function (err) {
   if (err) {
     console.log(err);
   }
   console.log(`My express server listening at http://localhost:${PORT}`);
 });
+
+//db tables creation
 
 serverApp.get("/install", function (req, res) {
   //create product table
@@ -95,4 +116,54 @@ serverApp.get("/install", function (req, res) {
   });
 });
 
-serverApp.use("/form", express.static("display"));
+//route static form template or html
+
+serverApp.use("/form", express.static("form"));
+
+//route post of form data
+
+serverApp.post("/addiphone", (req, res) => {
+  res.send("trying to reseive data");
+  console.table(req.body);
+  const {
+    product_name,
+    product_url,
+    product_descrpition,
+    product_brief_descrpition,
+    product_link,
+    product_image,
+    starting_price,
+    price_range,
+  } = req.body;
+
+  // to ensert product table
+  let insertToProduct = `INSERT INTO products (product_url, product_name) VALUES ("${product_url}","${product_name}")`;
+
+  con.query(insertToProduct, (err, result, fields) => {
+    if (err) console.log(`Error Found: ${err}`);
+  });
+
+  con.query(
+    `SELECT * FROM products WHERE product_name = "${product_name}"`,
+    (err, rows, fields) => {
+      // Extracting Foreign key
+
+      let product_id = rows[0].product_id;
+
+      let insertToDescription = `INSERT INTO productdescription (product_id,product_brief_description, product_description, product_img, product_link) VALUES ("${product_id}", "${product_brief_descrpition}","${product_descrpition}","${product_image}","${product_link}")`;
+
+      let inserttoPrice = `INSERT INTO productprice ( product_id,starting_price,price_range) VALUES ("${product_id}", "${starting_price}","${price_range}")`;
+
+      //  enserting in to productdescirption table and price table
+
+      con.query(insertToDescription, (err, result, fields) => {
+        if (err) console.log(`Error Found: ${err}`);
+      });
+      con.query(inserttoPrice, (err, result, fields) => {
+        if (err) console.log(`Error Found: ${err}`);
+      });
+    }
+  );
+  res.end("Data inserted to tables");
+  console.log("Data inserted to tables");
+});
